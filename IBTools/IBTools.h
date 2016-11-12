eac342cbbb56a5cf90e20278a94f31d8df148586
Hugo Gonzalez <hugo.gonzales@mirpartners.com>
2016-11-12 15:53:04 -0800

    Initial Commit

---
+++ IBTools/IBTools.h
@@ -0,0 +1,19 @@
+//
+//  IBTools.h
+//  IBTools
+//
+//  Created by hugogonzalez on 11/12/16.
+//  Copyright © 2016 Cat Bakery. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for IBTools.
+FOUNDATION_EXPORT double IBToolsVersionNumber;
+
+//! Project version string for IBTools.
+FOUNDATION_EXPORT const unsigned char IBToolsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <IBTools/PublicHeader.h>
+
+
