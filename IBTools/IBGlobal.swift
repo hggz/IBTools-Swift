88cf3a9e1b8888f1b0773916390c9e055262bbf2
Hugo Gonzalez <hagonzalez94@gmail.com>
2016-11-23 13:30:22 -0800

    Access specifier update.

--- IBTools/IBGlobal.swift
+++ IBTools/IBGlobal.swift
@@ -9,11 +9,11 @@
 import UIKit
 
 // MARK: - Screen
-let IS_IPAD = UIDevice.current.userInterfaceIdiom == .pad
-let SCREEN_SIZE = UIScreen.main.bounds.size
-let SCREEN_WIDTH = SCREEN_SIZE.width
-let SCREEN_HEIGHT = SCREEN_SIZE.height
-let SCREEN_SCALE = (SCREEN_WIDTH / 320.0)
-func SCALED_SCREEN_SIZE(value: CGFloat) -> CGFloat {
+public let IS_IPAD = UIDevice.current.userInterfaceIdiom == .pad
+public let SCREEN_SIZE = UIScreen.main.bounds.size
+public let SCREEN_WIDTH = SCREEN_SIZE.width
+public let SCREEN_HEIGHT = SCREEN_SIZE.height
+public let SCREEN_SCALE = (SCREEN_WIDTH / 320.0)
+public func SCALED_SCREEN_SIZE(value: CGFloat) -> CGFloat {
     return value * SCREEN_SCALE
 }
